822aab88942825c2cc03d7803a336c0ad4ac7259
kashi-x <kashimiya.exe@gmail.com>
2024-04-15 00:37:14 +0900

    add some

---
+++ clame.md
@@ -0,0 +1,4 @@
+<a href="text/chapter-15.xhtml"><span epub:type="z3998:roman">XV</span>: Back in Petrograd</a>
+
+We can not use this epub:type
+`//a/span@epub:type not work.`
--- src/data_adapter/standard_ebook_toc.py
+++ src/data_adapter/standard_ebook_toc.py
@@ -4,12 +4,12 @@ from dataclasses import asdict
 from dataclasses import dataclass
 from dataclasses import field
 from pathlib import Path
-from typing import Optional
+from typing import Self
 
 import structlog
 from lxml import etree
-from lxml.etree import Element
 from lxml.etree import HTMLParser
+from lxml.etree import _Element as Element
 from structlog.stdlib import BoundLogger
 
 from utils.logger_config import configure_logger
@@ -24,21 +24,23 @@ namespaces = {"xhtml": "http://www.w3.org/1999/xhtml"}
 class Chapter:
     """input data format is like this <a href="text/chapter-3.xhtml"><span epub:type="z3998:roman">III</span>: The Conference</a>."""
 
-    title: str
+    title: str = field(init=False)
+    number: str
+    name: str
     href: str
     nest_level: int
-    span: str
     query: str
     url: str
-    subchapters: list["Chapter"] = field(default_factory=list)
+    subchapters: list[Self | Element] = field(default_factory=list)
 
     def __post_init__(self):
         self.subchapters = process_raw_chapters_into_formated(
-            self.subchapters,
-            self.nest_level + 1,
-            self.url,
-            self.query + "/ol/li" if self.query else "",
+            raw_chapters=self.subchapters,  # type: ignore
+            query=self.query + "/ol/li",
+            nest_level=self.nest_level + 1,
+            url=self.url,
         )
+        self.title = "".join(self.number + self.name)
 
     def to_dict(self):
         if self.subchapters:
@@ -55,7 +57,8 @@ def find_root(xml_data: bytes) -> Element:
 
 
 def parse_from_xml_data(query: str, xml_data: bytes) -> list[Element]:
-    return find_root(xml_data).xpath(query)
+    # ANNOTATE: In fact, return value is lxml.etree._XpathObject, but list[Element] is more useful for me. Because XpathObject is too wide like str, int, None, Element, list[Element]..etc. But if you pass correct query, the result should be list of Element.
+    return find_root(xml_data).xpath(query)  # type: ignore
 
 
 def find_file_title(xml_data: bytes) -> str | None:
@@ -67,24 +70,33 @@ def find_chapters(xml_data: bytes) -> list[Element]:
 
 
 def process_raw_chapters_into_formated(
-    raw_chapters, nest_level=0, url="", query=None
+    raw_chapters: list[Element], query: str, nest_level=0, url=""
 ) -> list[Chapter]:
     return [
-        Chapter(
-            title=raw_chapter.xpath("a/text()")[0] if raw_chapter.xpath("a/text()") else "",
-            href=raw_chapter.xpath("a/@href")[0] if raw_chapter.xpath("a/@href") else "",
-            # //section[@id='chapter-39']/@id
-            # //section[@id='chapter-39']/@epub:type
-            nest_level=nest_level,
-            span=raw_chapter.xpath("a/span/text()")[0]
-            if raw_chapter.xpath("a/span/text()")
-            else "",
-            query=query + f"[{index}]" if query else "",
-            url=url,
-            subchapters=raw_chapter.xpath("ol/li"),
-        )
+        make_chapter(raw_chapter, query, nest_level, url, index)
         for index, raw_chapter in enumerate(raw_chapters, start=1)
     ]
+
+
+def make_chapter(a_element: Element, query: str, nest_level: int, url: str, index: int) -> Chapter:
+    def get_query_result_with_check(element: Element, query: str) -> str:
+        if result := element.xpath(query):
+            return result[0]  # type: ignore # reason at parse_from_xml_data
+        return ""
+
+    return Chapter(
+        number=get_query_result_with_check(a_element, "a/span/text()"),
+        name=get_query_result_with_check(a_element, "a/text()"),
+        href=get_query_result_with_check(a_element, "a/@href"),
+        nest_level=nest_level,
+        query=query + f"[{index}]" if query else "",
+        url=url,
+        subchapters=a_element.xpath("ol/li"),  # type: ignore
+    )
+
+
+# //section[@id='chapter-39']/@id
+# //section[@id='chapter-39']/@epub:type
 
 
 def create_dict_formated_chapters(chapters):
@@ -93,11 +105,11 @@ def create_dict_formated_chapters(chapters):
 
 def create_formated_chapters(xml_data: bytes) -> list[Chapter]:
     return process_raw_chapters_into_formated(
-        find_chapters(xml_data), query="//html/body/nav[@id='toc'][1]/ol/li"
+        find_chapters(xml_data), "//html/body/nav[@id='toc'][1]/ol/li"
     )
 
 
-def show(element):
+def show(element: Element | list[Element]) -> None:
     # This is for debugging.
     if len(element) == 0:
         print("No elements are found.")
@@ -107,12 +119,10 @@ def show(element):
         for _i, e in enumerate(element, 1):
             print(f"{_i:=^30}")
             show(e)
-    else:
+    if isinstance(element, Element):
         print(etree.tostring(element, pretty_print=True, encoding=str))
-
-
-# from pprint import pprint
-# pprint(chapters)
+    else:
+        print("No elements are found.")
 
 
 def tutorial():
@@ -127,12 +137,12 @@ def tutorial():
         json.dump([c.to_dict() for c in chapters], fp)
 
 
-def process_repo(repo_dir: Path, logger: BoundLogger) -> None:
+def process_repo(repo_dir: Path, logger: BoundLogger, force=False) -> None:
     if not (repo_dir / "toc.xhtml").exists():
         logger.warning("toc.xhtml does not exist", repo_path=repo_dir)
         return
 
-    if (repo_dir / "toc.json").exists():
+    if (repo_dir / "toc.json").exists() and not force:
         logger.info("toc.json already exists", repo_path=repo_dir)
         return
 
@@ -157,7 +167,7 @@ def main():
 
     for repo_dir in list(BOOK_DIR.glob("*")):
         if repo_dir.is_dir():
-            process_repo(repo_dir, logger)
+            process_repo(repo_dir, logger, force=True)
 
 
 if __name__ == "__main__":
